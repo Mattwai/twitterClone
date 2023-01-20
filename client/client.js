b1639b19d7d90db0b2fd1ffbb7842762accc42b0
mattwai <mattjwai17@gmail.com>
2023-01-20 14:19:44 +1300

    feat add display messages and rate limit

--- client/client.js
+++ client/client.js
@@ -2,32 +2,67 @@ console.log("hello world")
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const messagesElement = document.querySelector('.messages');
 const API_URL = 'http://localhost:5000/messages'
 
 loadingElement.style.display = 'none';
 
+listAllMessages();
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     const formData = new FormData(form);
-
-    // wahen form submitted, grab content
     const name = formData.get('name');
     const content = formData.get('content');
-
-    // store form content in an object
+  
     const message = {
-        name,
-        content
+      name,
+      content
     };
+  
     form.style.display = 'none';
     loadingElement.style.display = '';
-
-    // send data to backend
+  
     fetch(API_URL, {
-        method: 'POST',
-        body: JSON.stringify(message),
-        headers:{
-            'content-type': 'application/json'
-        }
-    });
-});
+      method: 'POST',
+      body: JSON.stringify(message),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }).then(response => response.json())
+      .then(createdMessage => {
+        form.reset();
+        setTimeout(() => {
+          form.style.display = '';
+        }, 30000);
+        listAllMessages();
+      });
+  });
+
+function listAllMessages() {
+    messagesElement.innerHTML = '';
+    fetch(API_URL)
+      .then(response => response.json())
+      .then(messages => {
+        messages.reverse();
+        messages.forEach(message => {
+          const div = document.createElement('div');
+  
+          const header = document.createElement('h3');
+          header.textContent = message.name;
+  
+          const contents = document.createElement('p');
+          contents.textContent = message.content;
+  
+          const date = document.createElement('small');
+          date.textContent = new Date(message.created);
+  
+          div.appendChild(header);
+          div.appendChild(contents);
+          div.appendChild(date);
+  
+          messagesElement.appendChild(div);
+        });
+        loadingElement.style.display = 'none';
+      });
+  }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const monk = require('monk');
+const rateLimit = require("express-rate-limit");
 
 const app = express();
+
+const db = monk('localhost/messages');
+const messages = db.get('messages');
 
 app.use(cors());
 app.use(express.json());
@@ -9,11 +14,46 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.json({
         message: 'sent!'
-    })
-})
+    });
+});
+
+app.get('/messages', (req, res) => {
+    messages
+      .find()
+      .then(messages => {
+        res.json(messages);
+      });
+  });
+
+function isValidMessage(message){
+    return message.name && message.name.toString().trim() !== '' && message.content && message.content.toString().trim() !== '';
+}
+
+app.use(rateLimit({
+    windowMs: 10 * 1000, // 10 seconds
+    max: 1
+  }));
 
 app.post('/messages', (req,res)=>{
-    console.log(req.body);
+    if(isValidMessage(req.body)){
+        const message = {
+            name: req.body.name.toString(),
+            content: req.body.content.toString(),
+            created: new Date()
+        };
+
+        messages
+        .insert(messages)
+        .then(createdMessage => {
+          res.json(createdMessage);
+        });
+    }
+    else{
+        res.status(422);
+        res.json({
+            message: 'Name and content is required!'
+        })
+    }
 });
 
 app.listen(5000, () => {
